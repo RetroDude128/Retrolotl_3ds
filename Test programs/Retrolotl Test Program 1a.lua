b9088baebb19707e20e55df9d8d32b927f6de33a
RetroBoi128 <64989222+RetroDude128@users.noreply.github.com>
2022-03-31 18:09:41 -0400

    Update Retrolotl Test Program 1a.lua

--- Test programs/Retrolotl Test Program 1a.lua
+++ Test programs/Retrolotl Test Program 1a.lua
@@ -1,10 +1,13 @@
 function love.draw()
+	touched = 0
 	touches = love.touch.getTouches( )
 	if next(touches) ~= nil then
 		x, y = love.touch.getPosition( touches[1] )
+		touched = 1
 	end
-    love.graphics.print(x, 100, 80)
+    	love.graphics.print(x, 100, 80)
 	love.graphics.print(y, 100, 120)
+	love.graphics.print(touched, 100, 60)
 end
 
 -- we need to quit the app when a button is pressed
