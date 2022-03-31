ceb7f61bcd26d07c5bf663886c2caba47430b711
RetroBoi128 <64989222+RetroDude128@users.noreply.github.com>
2022-03-31 15:40:19 -0400

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,14 @@
+
+function love.draw()
+	touches = love.touch.getTouches( )
+	if next(love.touch.getTouches()) ~= nil then
+		x, y = love.touch.getPosition( touches[1] )
+	end
+    love.graphics.print(x, 100, 80)
+	love.graphics.print(y, 100, 120)
+end
+
+-- we need to quit the app when a button is pressed
+function love.gamepadpressed(joystick, button)
+    love.event.quit()
+end
