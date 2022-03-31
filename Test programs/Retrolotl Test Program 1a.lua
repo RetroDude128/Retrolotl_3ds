78f6c85d07fa077fe374d57827d6dcd3344ffe22
RetroBoi128 <64989222+RetroDude128@users.noreply.github.com>
2022-03-31 18:29:45 -0400

    Update Retrolotl Test Program 1a.lua

--- Test programs/Retrolotl Test Program 1a.lua
+++ Test programs/Retrolotl Test Program 1a.lua
@@ -1,12 +1,13 @@
-x, y = 0
+-- Test Program 1
 function love.draw()
+	x, y = 0
 	touched = 0
 	touches = love.touch.getTouches( )
 	if next(touches) ~= nil then
 		x, y = love.touch.getPosition( touches[1] )
 		touched = 1
 	end
-    	love.graphics.print(x, 100, 80)
+    love.graphics.print(x, 100, 80)
 	love.graphics.print(y, 100, 120)
 	love.graphics.print(touched, 100, 60)
 end
