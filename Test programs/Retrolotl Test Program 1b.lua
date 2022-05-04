dbb97ab7ea4e65cd58ed789fef021674a6a397e6
RetroBoi128 <64989222+RetroDude128@users.noreply.github.com>
2022-05-04 19:12:15 -0400

    Update Retrolotl Test Program 1b.lua

--- Test programs/Retrolotl Test Program 1b.lua
+++ Test programs/Retrolotl Test Program 1b.lua
@@ -1,1 +1,18 @@
+-- Test Program 1
+function love.draw()
+	x, y = 0
+	touched = 0
+	touches = love.touch.getTouches( )
+	if next(touches) ~= nil then
+		x, y = love.touch.getPosition( touches[1] )
+		touched = 1
+		love.graphics.print(x, 100, 80)
+		love.graphics.print(y, 100, 120)
+	end
+	love.graphics.print(touched, 100, 60)
+end
 
+-- we need to quit the app when a button is pressed
+function love.gamepadpressed(joystick, button)
+    love.event.quit()
+end
