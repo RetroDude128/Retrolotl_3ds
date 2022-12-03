f84c6ec537270147733ec015d8fd1b8557387f87
RetroBoi128 <64989222+RetroDude128@users.noreply.github.com>
2022-12-03 18:35:18 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,14 +1,39 @@
+require ("controller")
+X = 0
+Y = 0
+XVel = 0
+YVel = 0
 
+love.graphics.setColor(255, 255, 255)
 function love.draw()
-	touches = love.touch.getTouches( )
-	if next(love.touch.getTouches()) ~= nil then
-		x, y = love.touch.getPosition( touches[1] )
-	end
-    love.graphics.print(x, 100, 80)
-	love.graphics.print(y, 100, 120)
-end
+    love.graphics.print("i exist", X, Y)
+    love.graphics.print(XVel, 100, 100)
+    love.graphics.print(YVel, 100, 120)
+end 
 
--- we need to quit the app when a button is pressed
-function love.gamepadpressed(joystick, button)
-    love.event.quit()
+function love.update(dt)
+    if Y < 120 then
+        YVel = YVel + 0.2
+    else
+        YVel = 0
+        Y = 120
+    end
+    if Control_InGet("a") then
+        YVel = -5
+    end
+    if math.abs(XVel) < 6 then
+        XVel = XVel + (Control_JoyGet(1) / 5)
+    end
+    if math.abs(YVel) < 6 then
+        YVel = YVel + (Control_JoyGet(2) / 5)
+    end
+    X = X + XVel
+    Y = Y + YVel
+    if XVel ~= 0 and math.abs(Control_JoyGet(1)) == 0 then
+        if XVel > 0 then
+            XVel = XVel - 0.1
+        else
+            XVel = XVel + 0.1
+        end
+    end
 end
